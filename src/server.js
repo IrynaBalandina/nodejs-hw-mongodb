f25284b22066610a062ff88d0c423346aba22e8b
Iryna <mefodievna2908@gmail.com>
2025-01-20 14:42:34 +0200

    api-docs

--- src/server.js
+++ src/server.js
@@ -8,18 +8,20 @@ import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import authRouter from "./routers/auth.js";
 // import { UPLOAD_DIR } from "./constants/index.js";
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
 dotenv.config();
 
 
 export const setupServer = ()=>{
     const app = express();
-
+    const { serve, setup } = swaggerDocs();
     app.use(cors());
 
     app.use(express.json());
     // app.use('/uploads', express.static(UPLOAD_DIR));
     app.use(express.static("uploads"));
+    app.use('/api-docs', serve, setup);;
     app.use(cookieParser());
      app.use(logger);
 
